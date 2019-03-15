fd2afd0aa10d2e4380816c47f4a29e9c853218e3
韩韵 <apple@hydemacbook-air.local>
2019-03-14 23:25:07 -0700

    Question 2: Finished the camera function.

--- Foodie/Foodie/ViewController.swift
+++ Foodie/Foodie/ViewController.swift
@@ -1,109 +1,52 @@
 //
 //  ViewController.swift
-//  SeeFood-CoreML
+//  Foodie
 //
-//  Created by Angela Yu on 27/06/2017.
-//  Copyright © 2017 Angela Yu. All rights reserved.
+//  Created by apple on 3/14/19.
+//  Copyright © 2019 EudoraHan. All rights reserved.
 //
 
 import UIKit
 import CoreML
 import Vision
-import Social
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+class ViewController: UIViewController, UIPickerViewDelegate, UINavigationControllerDelegate {
+
     @IBOutlet weak var imageView: UIImageView!
-    var classificationResults : [VNClassificationObservation] = []
     
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        imagePicker.delegate = self
-        
-    }
-    
-    func detect(image: CIImage) {
-        
-        // Load the ML model through its generated class
-        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
-            fatalError("can't load ML model")
-        }
-        
-        let request = VNCoreMLRequest(model: model) { request, error in
-            guard let results = request.results as? [VNClassificationObservation],
-                let topResult = results.first
-                else {
-                    fatalError("unexpected result type from VNCoreMLRequest")
-            }
-            
-            if topResult.identifier.contains("hotdog") {
-                DispatchQueue.main.async {
-                    self.navigationItem.title = "Hotdog!"
-                    self.navigationController?.navigationBar.barTintColor = UIColor.green
-                    self.navigationController?.navigationBar.isTranslucent = false
-                    
-                    
-                }
-            }
-            else {
-                DispatchQueue.main.async {
-                    self.navigationItem.title = "Not Hotdog!"
-                    self.navigationController?.navigationBar.barTintColor = UIColor.red
-                    self.navigationController?.navigationBar.isTranslucent = false
-                    
-                }
-            }
-            
-            
-        }
-        
-        let handler = VNImageRequestHandler(ciImage: image)
-        
-        do { try handler.perform([request]) }
-        catch { print(error) }
-        
-        
-        
-    }
-    
-    
-    
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        
-        if let image = info[.originalImage] as? UIImage {
-            
-            imageView.image = image
-            
-            imagePicker.dismiss(animated: true, completion: nil)
-            
-            
-            guard let ciImage = CIImage(image: image) else {
-                fatalError("couldn't convert uiimage to CIImage")
-            }
-            
-            detect(image: ciImage)
-            
-        }
-    }
-    
-    
-    @IBAction func cameraTapped(_ sender: Any) {
-        
+       
+        imagePicker.delegate = self as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
         
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+         
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+       
+        
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
         present(imagePicker, animated: true, completion: nil)
+        
+        
+        
+        
     }
     
 }
 
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-    return input.rawValue
-}
